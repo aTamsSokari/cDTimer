8250f81c44fb1335333a31c99421a238a22bc73b
Tams Sokari <atamssokari@yahoo.com>
2015-03-17 01:40:51 +0100

    a countdown timer controller in angular.js, called by setting duration in minutes

---
+++ cDTimer.js
@@ -0,0 +1,41 @@
+"use strict";
+
+angular.module("cDT", [])
+	.controller("timerController", function ($timeout, $window) {
+		var me = this;
+
+		var tick = function () {
+
+			if (time <= 0) {
+				$window.alert("time is up");
+				return;
+			}
+			time -= 1;
+			updateTimer();
+			$timeout(tick, 1000);
+
+		};
+
+		var time;
+
+		me.startTimer = function (duration) {
+			time = duration * 60;
+			time -= 1;
+			updateTimer();
+			$timeout(tick, 1000);
+		};
+
+		var updateTimer = function() {
+			var seconds = time,
+				minutes = Math.floor(seconds / 60);
+			seconds -= minutes * 60;
+			var	hours = Math.floor(seconds / 3600);
+			seconds -= hours * 3600;
+			me.timeStr = leadingZero(hours) + " : " + leadingZero(minutes) + " : " + leadingZero(seconds);
+		};
+
+		var leadingZero = function (num) {
+			return (num < 10) ? "0" + num : + num;
+		};
+
+	});
